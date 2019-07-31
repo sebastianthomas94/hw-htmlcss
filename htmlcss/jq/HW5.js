0e4331fbb64781b3889ac2a324d1782c400c1ee9
Sebastianthomas94 <Sebastianthomas94@outlook.com>
2019-07-31 10:31:53 +0530

    updates

--- htmlcss/jq/HW5.js
+++ htmlcss/jq/HW5.js
@@ -3,22 +3,24 @@ $(document).ready(function()
 
         $.ajax({
             type:"GET",
-            url:"https://newsapi.org/v2/everything?q=bitcoin&from=2019-06-30&sortBy=publishedAt&apiKey=ed023e439dde4269a688277a494cd8be",
+            url:"https://newsapi.org/v2/top-headlines?country=in&category=&apiKey=b10e2e754b6c4f658f64feeb789d2c3b",
 
             beforeSend: function()
                { $("#loader").show();},
             
            success:function(data)
-            { 
-                console.log(data);
-                var output='<div class="card container" style="margin-top: 30px">';
-                for(var i in 70)
-                    {
-                        $("#loader").hide();
-                        output+='<h5 class="card-header" id="head">'+data.article[i].title+'</h5>';
-                        output+='<div class="card-body"> <img src="'+article[i].urlToImage+'>';
-                        output+='<p class="card-text">'+article[i].description+'</p>';
-                        output+='<a href="'+article[i].urlToImage+'" class="btn btn-primary">Read More</a></div></div>';
+            {   $("#loader").hide();
+                //console.log(data);
+                //alert(data['articles'][1].title);
+                var output="";
+                for(var i in data['articles']);
+                    {   alert(data['articles'][i].title);
+                        //console.log(data['articles'][i].title);
+                        output='<div class="card container" style="margin-top: 30px">';
+                        output+='<h5 class="card-header" id="head">'+data['articles'][i].title+'</h5>';
+                        output+='<div class="card-body"> <img src="'+data['articles'][i].urlToImage+' height="600" width="600">';
+                        output+='<p class="card-text">'+data['articles'][i].description+'</p>';
+                        output+='<a href="'+data['articles'][i].urlToImage+'" class="btn btn-primary">Read More</a></div></div>';
                         
                     }
                 $("#result").html(output);
